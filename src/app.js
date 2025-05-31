efd0b07c801039bd97c29f2c6d565f4c9b039a1d
Violacordis <ikemviolacordis@gmail.com>
2025-05-31 17:18:34 +0100

    feat/branch: create, validate

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const prisma = require("../db/prisma.js"); // Adjust the path as necessary
+const branchRoute = require("./route/branch.route.js");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/v1/branches", branchRoute);
 
 app.get("/api/v1", async (req, res) => {
   return res.status(200).json({
