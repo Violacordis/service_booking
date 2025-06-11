d37b349260f70cd31cc5252aee3c269329c9ad5c
Violacordis <ikemviolacordis@gmail.com>
2025-06-11 23:02:17 +0100

    remove dist git track

--- dist/src/common/middleware/not-found.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = void 0;
-const app_error_1 = require("../errors/app.error");
-const notFoundHandler = (req, res, next) => {
-    next(new app_error_1.AppError('Route not found', 404));
-};
-exports.notFoundHandler = notFoundHandler;
